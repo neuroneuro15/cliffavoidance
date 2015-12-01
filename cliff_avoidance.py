a38ac8ea8a3d6632a3edce9101c16225d19b6927
Nicholas A. Del Grosso, Lab Comp <delgrosso.nick@gmail.com>
2015-12-01 20:47:12 +0100

    got rid of stupid virtual board

--- cliff_avoidance.py
+++ cliff_avoidance.py
@@ -32,8 +32,9 @@ arena = arena_reader.get_mesh('Arena', lighting=True, centered=False)
 
 # Import Cliff Avoidance objects
 reader = rc.graphics.WavefrontReader(path.join('Objects', 'CliffAvoidance.obj'))
-vir_meshes = reader.get_meshes(['FakeArena', 'Board', 'DepthLeft', 'DepthRight'])
+vir_meshes = reader.get_meshes(['FakeArena', 'DepthLeft', 'DepthRight'])
 [vir_meshes[name].load_texture(rc.graphics.resources.img_uvgrid) for name in vir_meshes]  # Put an image texture on the walls and floors
+board = reader.get_mesh('Board')
 
 # Use a Pseudo-Random order for determining which side the deep floor should be on.
 floor_to_change = vir_meshes['DepthRight'] if metadata['Cliff Side T-R, F-L'] else vir_meshes['DepthLeft']
@@ -41,10 +42,10 @@ floor_to_change.local.y -= metadata['Cliff Depth']
 
 # Realign everything to the arena, for proper positioning
 additional_rotation = rc.utils.correct_orientation_natnet(arena_rb)
-rc.utils.update_world_position_natnet(vir_meshes.values() + [arena], arena_rb, additional_rotation)
+rc.utils.update_world_position_natnet(vir_meshes.values() + [arena, board], arena_rb, additional_rotation)
 
 # Build ratCAVE Scenes
-active_scene = rc.graphics.Scene([arena, vir_meshes['Board']], bgColor=(0., .3, 0., 1.),
+active_scene = rc.graphics.Scene([arena, board], bgColor=(0., .3, 0., 1.),
                                  camera=rc.graphics.projector, light=rc.graphics.projector)
 
 virtual_scene = rc.graphics.Scene(vir_meshes.values(), light=rc.graphics.projector, bgColor = (.1, 0., .1, 1.))
